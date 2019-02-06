df9c8cb270d5e2d9ca862126a7f97f2306ce04ed
Abraham Kamau <kirumba.kamau@gmail.com>
2019-02-06 11:30:26 +0300

    [starts #163441722] Added upvote and downvote

--- ui/js/one_question.js
+++ ui/js/one_question.js
@@ -29,6 +29,7 @@ function viewQuestion() {
                     document.getElementById('description').innerHTML = JSON.stringify(data.data[2]).toString().replace(/"/g, "");
                     document.getElementById('votes').innerHTML = JSON.stringify(data.data[6]).toString().replace(/"/g, "");
                     document.getElementById('upvote').addEventListener("click", upvote)
+                    document.getElementById('downvote').addEventListener("click", downvote)
             }
             else {
                 window.alert(data.message);
@@ -59,7 +60,39 @@ function upvote() {
     })
         .then((response) => response.json())
         .then((data => {
-            if (data.status === 201) {
+            if (data.status === 200) {
+                window.location = window.location
+            }
+            else {
+                window.alert(data.message);
+            }
+        }))
+}
+
+function downvote() {
+    /*
+    Function to collect one Question
+    */
+
+    var page_URL = document.URL;
+    var split_url = page_URL.split('/')
+    var last_part = split_url[split_url.length - 1]
+    var split_last = last_part.split('=')
+    var qid = split_last[split_last.length - 1]
+
+    const prefix = 'http://127.0.0.1:5000/api/v2';
+    const url = prefix + '/questions/' + qid + '/downvote/';
+
+    fetch(url, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.getItem('token')
+        }
+    })
+        .then((response) => response.json())
+        .then((data => {
+            if (data.status === 200) {
                 window.location = window.location
             }
             else {
