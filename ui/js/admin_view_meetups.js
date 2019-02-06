b8b6eb6ac13ae562824260ef4bbd95b5e9d8263b
Abraham Kamau <kirumba.kamau@gmail.com>
2019-02-07 02:00:02 +0300

    [finishes #163441721] Created fetch for delete meetup

--- ui/js/admin_view_meetups.js
+++ ui/js/admin_view_meetups.js
@@ -24,6 +24,7 @@ function viewMeetups() {
                                 <p id="more">${truncate(JSON.stringify(meetup[2]).toString().replace(/"/g, ""), 28)} <a class="url-link" href="#" id="${JSON.stringify(meetup[0])}"> ..more</a></p>
                                 <ul>
                                     <li class="user-item">
+                                        <button class="btn-tags" id="delete${JSON.stringify(meetup[0])}">Delete</button>
                                         <p class="user">Posted
                                             2hrs ago<a href="#" class="url-link link"> By Admin</a></p>
                                     </li>
@@ -35,8 +36,36 @@ function viewMeetups() {
                     let url_id = document.getElementById(JSON.stringify(meetup[0])).id
                     var url = "../ui/meetup.html?meet=" + encodeURIComponent(url_id)
                     document.getElementById(JSON.stringify(meetup[0])).addEventListener("click", meetup_redirect)
+                    document.getElementById('delete' + JSON.stringify(meetup[0])).addEventListener("click", delete_meetup)
                     function meetup_redirect() {
                         window.location.href = url
+                    }
+                    function delete_meetup(event) {
+
+                        event.preventDefault();
+
+                        let mid = JSON.stringify(meetup[0])
+
+                        const prefix = 'http://127.0.0.1:5000/api/v2';
+                        const url = prefix + '/meetups/' + mid + '/delete/';
+
+                        fetch(url, {
+                            method: 'DELETE',
+                            headers: {
+                                'Content-Type': 'application/json',
+                                'Authorization': localStorage.getItem('token')
+                            }
+                        })
+                            .then((response) => response.json())
+                            .then((data => {
+                                if (data.status === 201) {
+                                    window.alert(data.message)
+                                    window.location = window.location
+                                }
+                                else {
+                                    window.alert(data.message);
+                                }
+                            }))
                     }
                 });
             }
